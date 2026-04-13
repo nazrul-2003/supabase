e6355b5848d5b78fbac13a7fceb3935a2392d6bc
Mohammad Nazrul Islam <162697930+nazrul-2003@users.noreply.github.com>
2026-04-14 01:09:04 +0600

    Delete apps directory

--- apps/studio/components/interfaces/TableGridEditor/SidePanelEditor/SpreadsheetImport/SpreadsheetImport.types.ts
+++
@@ -1,9 +0,0 @@
-import type { InferredColumnType } from './SpreadsheetImport.utils'
-import type { Dictionary } from '@/types'
-
-export interface SpreadsheetData {
-  headers: string[]
-  rows: unknown[]
-  rowCount: number
-  columnTypeMap: Dictionary<InferredColumnType>
-}
--- apps/studio/data/read-replicas/replicas.utils.ts
+++
@@ -1,8 +0,0 @@
-import { last } from 'lodash'
-
-import { AVAILABLE_REPLICA_REGIONS } from '@/components/interfaces/Settings/Infrastructure/InfrastructureConfiguration/InstanceConfiguration.constants'
-
-export const formatDatabaseID = (id: string) => last(id.split('-') ?? [])
-
-export const formatDatabaseRegion = (region: string) =>
-  last(AVAILABLE_REPLICA_REGIONS.find((r) => r.region === region)?.name.split('('))?.split(')')[0]
