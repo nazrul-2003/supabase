197984adfd013a9b948615c48fbc923f62ff5ce2
Mohammad Nazrul Islam <162697930+nazrul-2003@users.noreply.github.com>
2026-04-14 01:11:08 +0600

    Delete blocks/vue directory

--- blocks/vue/registry/dropzone.ts
+++
@@ -1,6 +0,0 @@
-import { type RegistryItem } from 'shadcn/schema'
-
-import nuxtjs from './default/dropzone/nuxtjs/registry-item.json' with { type: 'json' }
-import vue from './default/dropzone/vue/registry-item.json' with { type: 'json' }
-
-export const dropzone = [nuxtjs, vue] as RegistryItem[]
--- blocks/vue/registry/realtime-avatar-stack.ts
+++
@@ -1,6 +0,0 @@
-import { type RegistryItem } from 'shadcn/schema'
-
-import nuxtjs from './default/realtime-avatar-stack/nuxtjs/registry-item.json' with { type: 'json' }
-import vue from './default/realtime-avatar-stack/vue/registry-item.json' with { type: 'json' }
-
-export const realtimeAvatarStack = [nuxtjs, vue] as RegistryItem[]
